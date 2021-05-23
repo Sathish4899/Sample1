09be1232510a13f56dda76ec35d375f84f06932f
sathish4899 <sathishmorrison95@gmail.com>
2021-05-23 19:50:03 +0530

    Initial commit

---
+++ Git Project/src/Factorial.java
@@ -0,0 +1,21 @@
+
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int num=5;
+		int fact=num;
+		while(num>1)
+		{
+			
+	     num=num-1;	
+	     fact=fact*num;
+	     
+		// TODO Auto-generated method stub
+
+	}
+		System.out.println(fact);
+
+}
+}
